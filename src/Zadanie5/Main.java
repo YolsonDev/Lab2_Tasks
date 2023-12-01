29aa28095e3856066270ff6c837841aebded387c
YolsonDev <yolsondev@gmail.com>
2023-12-01 01:42:59 +0100

    Initial commit

--- src/Zadanie5/Main.java
+++ src/Zadanie5/Main.java
@@ -2,12 +2,5 @@ package Zadanie5;
 
 public class Main {
     public static void main(String[] args) {
-        Samochod samochod = new Samochod();
-        System.out.println("Dane o samochodzie: " +
-                samochod.producent + " " +
-                samochod.model + " " +
-                samochod.kolor + " " +
-                samochod.rocznik);
-
     }
 }
--- src/Zadanie5/Samochod.java
+++ src/Zadanie5/Samochod.java
@@ -1,8 +1,9 @@
 package Zadanie5;
 
 public class Samochod {
-    String producent = "Mercedes";
-    String model = "CLA";
-    String kolor = "Czarny";
-    int rocznik = 2016;
+    String producent;
+    String model;
+    String kolor;
+    int rocznik;
+    int przebieg;
 }
